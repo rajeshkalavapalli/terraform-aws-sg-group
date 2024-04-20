2272c943398d0dc50a1b6414163ff2b60fbc25a7
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-04-20 21:10:52 +0530

    sg1

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "sg_name" {
+  
+}
+
+variable "sg_description" {
+  
+
+}
+
+variable "project_name" {
+   
+  
+}
+
+variable "environment" {
+
+}
+
+variable "common_tags" {
+  type = map 
+  
+}
+
+variable "sg_tags" {
+  
+}
